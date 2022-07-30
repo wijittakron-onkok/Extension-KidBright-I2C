7b78c23de09d2ab47620fd4dd43428cab8996ab4
Wijittakron Onkok <wijittakron@taladwaree.ac.th>
2022-07-30 19:06:48 +0700

    Initial commit

---
+++ extension.js
@@ -0,0 +1,13 @@
+({
+  name: "I2C Scanner",
+  description: "KidBright V1.3 I2C Scanner",
+  author: "WIJITTAKRON",
+  category: "Other",
+  version: "1.0.0",
+  icon: "/static/icon.png",
+  color: "#985BA5",
+  blocks: [
+    "kb_i2c_scanner_decimal",
+    "kb_i2c_scanner_hexadecimal"
+  ]
+});
